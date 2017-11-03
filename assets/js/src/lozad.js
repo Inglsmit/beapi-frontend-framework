be12ddc611f946feddc1e7db7a89b5f29705e123
Johanna Khenkittisak <jkhenkittisak@beapi.fr>
2017-11-03 17:47:31 +0100

    Add polyfill to lozad

--- assets/js/src/lozad.js
+++ assets/js/src/lozad.js
@@ -1,6 +1,7 @@
 /**
 * lazy loads elements with default selector as '.lozad'
 */
+require('intersection-observer')
 const lozad = require('lozad')
 
 const observer = lozad() // lazy loads elements with default selector as '.lozad'
